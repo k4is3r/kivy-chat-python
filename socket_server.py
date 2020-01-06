1e04901594c2d67bcd7f98d538f0e8f89352d6fe
Eduardo Imery <ejwimery@gmail.com>
2020-01-06 08:33:04 -0600

    server socket py

--- socket_server.py
+++ socket_server.py
@@ -14,4 +14,24 @@ server_socket.setsockpot(socket.SOL_SOCKET,
                          socket.SO_REUSEADDR,
                          1)
 
+server_socket.bind((IP,PORT))
+
+
+server_socker.listen()
+
+socker_list = [server_socket]
+
+clients = {}
+
+print(f'Listening for conections on {IP}:{PORT}...')
+
+def receive_message(client_socket):
+    
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+        if not len(message_header):
+            return False
+
+        message_length =  int(message_header.decode('utf-8').strip())
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
 
