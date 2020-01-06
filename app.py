f94e2cadad7563e59dc49f17fdd026e412ae1ce2
Eduardo Imery <ejwimery@gmail.com>
2020-01-06 07:06:26 -0600

    First run of app kivy

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ kivy.require('1.11.1')
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import T
+from kivy.uix.textinput import TextInput
+
+
+class ChatIpApp(App):
+    def build(self):
+        return Label(text='Hi from kivy')
+
+
+
+if __name__ == '__main__':
+    ChatIpApp().run()
 
