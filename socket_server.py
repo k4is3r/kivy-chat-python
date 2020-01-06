69a3a7897b1bfa8ec4502015004bf200422c5710
Eduardo Imery <ejwimery@gmail.com>
2020-01-06 08:27:20 -0600

    socket server creating varibales

--- socket_server.py
+++ socket_server.py
@@ -1,4 +1,17 @@
 import socket
 import select
 
+HEADER_LENGTH = 10
+
+IP = '127.0.0.1'
+PORT = 1234
+
+sever_socket =  socket.socket(socket.AF_INET,
+                              socket.SOCK_STEAM)
+
+
+server_socket.setsockpot(socket.SOL_SOCKET,
+                         socket.SO_REUSEADDR,
+                         1)
+
 
