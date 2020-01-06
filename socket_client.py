b8bee2ec04f4fd999d442bc34e1b8ec01424c015
Eduardo Imery <ejwimery@gmail.com>
2020-01-06 09:14:52 -0600

    socket client py modifing

--- socket_client.py
+++ socket_client.py
@@ -9,3 +9,18 @@ client_socket = None
 def connect(ip, port, my_username, error_callback):
 
     global client_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        
+        client_socket.connect((ip, port))
+    except Exception as e:
+        
+        error_callback('Connection error: {}'.format(str(e)))
+        return False
+    username = my_username.encode('utf-8')
+    username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(username_header + username)
+
+    return True
+
