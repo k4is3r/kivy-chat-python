ec858c88c721987596b489eeccacf3fd563181c1
Eduardo Imery <ejwimery@gmail.com>
2020-01-06 09:12:32 -0600

    creating the sockect client file

---
+++ socket_client.py
@@ -0,0 +1,11 @@
+import socket
+import errno
+from threading import Thread
+
+HEADER_LENGTH = 10
+client_socket = None
+
+# Connects to the server
+def connect(ip, port, my_username, error_callback):
+
+    global client_socket
